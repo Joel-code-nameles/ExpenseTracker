d91fc535e438dad19dc69d4096444045bd9813a6
Joel-codes-nameles <anunyam.asare@gmail.com>
2025-10-23 20:19:16 -0300

    commited

--- ETracker/TrackerApp/models.py
+++ ETracker/TrackerApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class TrackerApp(models.Model):
+    title = models.CharField(max_length=100)
+    amount = models.CharField(max_length=100)
+    date_registered = models.DateTimeField(auto_now_add=True)
+    categorizes = models.CharField(max_length=100)
+    notes = models.CharField(max_length=150)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title,self.notes
